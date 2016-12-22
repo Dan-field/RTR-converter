9d9a6b24ad2536dbb3e9e2a3fd35478b891f6685
Dan-field <daniel@fieldfam.com>
2016-12-22 22:45:02 +1000

    Add files via upload
    Further experimentation, found that a SKIP value in the 'fread' produces more promising-looking results.
    
    Suspect that the R&S file might use a uint14 format (based on some R&S documentation), which is not directly supported by the fread function.

--- OpenRTR.m
+++ OpenRTR.m
@@ -1,6 +1,8 @@
 % Open a specific file, then read it as a binary file.
 filename = "PR100/estate5m-01.rtr";
 fid = fopen(filename);
-Data = fread(fid, [32, Inf], "float");
-% NOTE: have not yet determined what precision to use
+% --- EXPERIMENTAL ---
+Data = fread(fid, [512, Inf], "uint32", 1);
+% result looks more like a tract if the SKIP value (4th argument) is odd
 fclose(fid);
+plot(Data(50,:));
