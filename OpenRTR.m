ea65db2200b19a75c6fae19904f5bbbc26917a57
Daniel Field <daniel@fieldfam.com>
2016-12-26 20:07:50 +1000

    Add files via upload
    I have started work on a manual 16-bit to 14-bit converter. Writing it out in long form at the moment; intending to optimise once the pattern becomes evident. Just does the first three elements so-far.

--- OpenRTR.m
+++ OpenRTR.m
@@ -2,7 +2,31 @@
 filename = "PR100/estate5m-01.rtr";
 fid = fopen(filename);
 % --- EXPERIMENTAL ---
-Data = fread(fid, [512, Inf], "uint16");
+data = fread(fid, [512, Inf], "uint16");
 fclose(fid);
-Selection = reshape(dec2hex(Data([1:16],[501:516])),32,32);
-% plot(Data(50,:));
+selection = data([1:16],[501:516]);
+first = selection(1,:);
+container = zeros(1, 16);
+for(i = 1:14)
+  container(1) = bitset(container(1), (15-i), bitget(first(1), 15-i));
+endfor
+for(i = 1:2)
+  container(2) = bitset(container(2), (3-i), bitget(first(1), 17-i));
+endfor
+for(i = 1:12)
+  container(2) = bitset(container(2), (15-i), bitget(first(2), 13-i));
+endfor
+for(i = 1:4)
+  container(3) = bitset(container(3), (5-i), bitget(first(2), 17-i));
+endfor
+for(i = 1:10)
+  container(3) = bitset(container(3), (15-i), bitget(first(3), 11-i));
+endfor
+printf("14-bit values: ")
+bitget(container(1), [1:16])
+bitget(container(2), [1:16])
+bitget(container(3), [1:16])
+printf("16-bit originals: ")
+bitget(first(1), [1:16])
+bitget(first(2), [1:16])
+bitget(first(3), [1:16])
